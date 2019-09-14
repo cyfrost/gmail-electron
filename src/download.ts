8c601b6d7f88995a770a84ee743a41a9d95c3280
Cyrus <cyrus.frost@hotmail.com>
2019-09-15 00:15:41 +0530

    [@markypython]  add download status notifications

---
+++ src/download.ts
@@ -0,0 +1,37 @@
+import { shell, app } from 'electron'
+import * as path from 'path'
+import electronDl from 'electron-dl'
+
+import { createNotification } from './notification'
+
+type State = 'cancelled' | 'completed' | 'interrupted'
+
+const messages = {
+  cancelled: 'has been cancelled',
+  completed: 'has completed',
+  interrupted: 'has been interrupted'
+}
+
+function onDownloadComplete(filename: string, state: State): void {
+  createNotification(
+    `Download ${state}`,
+    `Download of file ${filename} ${messages[state]}.`,
+    {
+      action: () => {
+        shell.openItem(path.join(app.getPath('downloads'), filename))
+      },
+      text: 'Open'
+    }
+  )
+}
+
+export function init(): void {
+  electronDl({
+    showBadge: false,
+    onStarted: item => {
+      item.on('done', (_, state) => {
+        onDownloadComplete(item.getFilename(), state)
+      })
+    }
+  })
+}
